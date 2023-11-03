822168466e31219505400e188a595cac85bb86f0
riteshm1hexaware <147592652+riteshm1hexaware@users.noreply.github.com>
2023-11-03 15:20:41 +0530

    Added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('HEADER')
-streamlit.text('Text')
+streamlit.header('Breasfast Menu')
+streamlit.text('Omeage 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
