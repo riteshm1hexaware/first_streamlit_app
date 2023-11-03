e9781495e52c8e3a32b22e536e41c845b7cbcc92
riteshm1hexaware <147592652+riteshm1hexaware@users.noreply.github.com>
2023-11-03 15:49:24 +0530

    Fruits to show

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,21 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Step 1 - list the fruits
 #streamlit.dataframe(my_fruit_list)
 
+#Step 3 - Allow fruit selection on the basis of name
 my_fruit_list=my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-streamlit.dataframe(my_fruit_list)
+
+#Step 2 - allow fruit selection
+#streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+
+#Step 4 - Allow pre-populated items
+#streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+#streamlit.dataframe(my_fruit_list)
+
+#Step 5 - Use variable names
+fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
