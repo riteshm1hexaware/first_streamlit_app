4bb83671603ed64f366b26f975779afe8d4a1d31
riteshm1hexaware <147592652+riteshm1hexaware@users.noreply.github.com>
2023-11-03 15:24:03 +0530

    Added one more item, and images

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breasfast Menu')
-streamlit.text('Omeage 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omeage 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
